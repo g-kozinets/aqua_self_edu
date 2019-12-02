c212e853bfcae9559e2085df7c640cfb5df16c28
g.kozinets <g.kozinets@a1qa.com>
2019-12-02 17:15:41 +0300

    added post model for api responses(not working yet)

--- vkAPI_Test/src/test/java/project/api/VkApi.java
+++ vkAPI_Test/src/test/java/project/api/VkApi.java
@@ -7,7 +7,6 @@ import org.json.JSONArray;
 import project.models.Post;
 
 import java.io.IOException;
-import java.util.ArrayList;
 import static project.enums.ApiMethod.*;
 import static project.enums.UrlParam.*;
 
